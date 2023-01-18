cdd5562cb94177802ce167fd1d477be0d8aadd10
dengpengc <dengpengc@opera.com>
2023-01-18 15:52:23 +0800

    add redis case

--- redis/pool.go
+++ redis/pool.go
@@ -9,15 +9,14 @@ import (
 
 var RedisPool *redis.Pool
 
-func NewRedisPool(server string, port int, maxIdle, maxActive int) *redis.Pool {
+func NewRedisPool(server string, port int, maxIdle, maxActive, db int) *redis.Pool {
 	RedisPool = &redis.Pool{
-		MaxIdle:   maxIdle,
-		MaxActive: maxActive,
+		MaxIdle:     maxIdle,
+		MaxActive:   maxActive,
+		IdleTimeout: time.Duration(5) * time.Second,
 		Dial: func() (redis.Conn, error) {
 			hostPort := fmt.Sprintf("%s:%d", server, port)
-			return redis.Dial("tcp", hostPort, redis.DialConnectTimeout(time.Millisecond*150),
-				redis.DialReadTimeout(time.Millisecond*150),
-				redis.DialWriteTimeout(time.Millisecond*150))
+			return redis.Dial("tcp", hostPort, redis.DialDatabase(db))
 		},
 	}
 	return RedisPool
