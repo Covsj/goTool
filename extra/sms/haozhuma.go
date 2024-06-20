a1436dd0f086cca1db06897c1eff16909eb5d8b7
dengpengc <dengpengc@opera.com>
2024-06-20 11:52:20 +0800

    [ad-hoc] add haozhuma sms support

---
+++ extra/sms/haozhuma.go
@@ -0,0 +1,99 @@
+package util
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/Covsj/goTool/http"
+	"github.com/Covsj/goTool/log"
+)
+
+type HaoZhuMaHandler struct {
+	token string
+}
+
+const (
+	domain = "https://api.haozhuma.com"
+)
+
+type LoginResp struct {
+	Msg   string `json:"msg"`
+	Code  int    `json:"code"`
+	Token string `json:"token"`
+}
+
+func (h *HaoZhuMaHandler) Login(user, password string) {
+	reqUrl := fmt.Sprintf("%s/sms/?api=login&user=%s&pass=%s", domain, user, password)
+	res := &LoginResp{}
+
+	resp, respBody, err := http.Get(reqUrl, res)
+	if err != nil {
+		log.Error("豪猪码登陆失败", "user", user, "password", password,
+			"Error", err.Error(), "Status", resp.Status, "RespBody", string(respBody))
+		return
+	}
+
+	log.Info("豪猪码登陆成功", "user", user, "password", password, "res", res)
+	h.token = res.Token
+}
+
+type PhoneResp struct {
+	Code        string      `json:"code"`
+	Msg         string      `json:"msg"`
+	Sid         string      `json:"sid"`
+	ShopName    string      `json:"shop_name"`
+	CountryName string      `json:"country_name"`
+	CountryCode string      `json:"country_code"`
+	CountryQu   string      `json:"country_qu"`
+	Uid         interface{} `json:"uid"`
+	Phone       string      `json:"phone"`
+	Sp          string      `json:"sp"`
+	PhoneGsd    string      `json:"phone_gsd"`
+}
+
+func (h *HaoZhuMaHandler) GetPhone(sid string) string {
+	reqUrl := fmt.Sprintf("%s/sms/?api=getPhone&token=%s"+
+		"&sid=%s&ascription=2&isp=&isp=&Province=&sp=2&paragraph=&isp=1", domain, h.token, sid)
+	for i := 0; i < 5; i++ {
+		res := &PhoneResp{}
+		resp, respBody, err := http.Get(reqUrl, res)
+		if err != nil {
+			log.Error("获取号码失败", "sid", sid,
+				"Error", err.Error(), "Status", resp.Status, "RespBody", string(respBody))
+			continue
+		}
+		mobile := res.Phone
+		if mobile != "" {
+			log.Info("获取手机号成功", "mobile", mobile)
+			return mobile
+		}
+	}
+	return ""
+}
+
+type MessageResp struct {
+	Code interface{} `json:"code,omitempty"`
+	Msg  string      `json:"msg,omitempty"`
+	Sms  string      `json:"sms,omitempty"`
+	Yzm  string      `json:"yzm,omitempty"`
+}
+
+func (h *HaoZhuMaHandler) GetMessage(sid, phone string) (string, string) {
+	reqUrl := fmt.Sprintf("%s/sms/?api=getMessage&token=%s&sid=%s&phone=%s", domain, h.token, sid, phone)
+	for i := 0; i < 20; i++ {
+		res := &MessageResp{}
+		resp, respBody, err := http.Get(reqUrl, res)
+		if err != nil {
+			log.Error("获取验证码失败", "sid", sid,
+				"Error", err.Error(), "Status", resp.Status, "RespBody", string(respBody))
+			continue
+		}
+		log.Info("正在获取验证码", "phone", phone, "res", res)
+		time.Sleep(5 * time.Second)
+		if res.Yzm != "" {
+			log.Info("获取验证码成功", "sid", sid, "mobile", phone, "code", res.Yzm)
+			return res.Yzm, res.Sms
+		}
+	}
+	return "", ""
+}
